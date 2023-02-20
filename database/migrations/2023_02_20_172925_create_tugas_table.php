836b7b71f5e63aa77882b7b881a59bc2c443fb33
Shafa Alafghany <alafghanyshafa@gmail.com>
2023-02-21 00:55:04 +0700

    adjust migration for tugas table

--- database/migrations/2023_02_20_172925_create_tugas_table.php
+++ database/migrations/2023_02_20_172925_create_tugas_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->integer('id', true);
             $table->integer('id_sesi')->index('fk_tugas_sesi');
             $table->string('judul', 100);
-            $table->integer('deskripsi');
+            $table->string('deskripsi', 255);
             $table->string('batas_pengumpulan', 15);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrentOnUpdate()->nullable();
